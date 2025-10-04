4adb8304e845d17915ed66703a0c930c31036b68
Pupton08 <upton_p@bryanston.co.uk>
2025-10-04 09:28:27 +0000

    finished

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System;
 class Program
 {
-    static void Main()
+
         static void Main(string[] args)
         {
             // declare an empty queue of size 10
@@ -45,50 +45,83 @@ class Program
 
 
     } // end Program class
-
+        
     public class Queue     
     {
-        // attributes
-        // maxSize;
-        // numItems;
-        // front;
-        // rear;
-        // array queueContents;
+        private int maxSize;
+        private int numItems;
+        private int front;
+        private int rear;
+        private int[] queueContents;
 
         // constructor (one argument)
         public Queue(int size)
         {
-    
+            front = 0;
+            rear = -1;
+            numItems = 0;
+            maxSize = size;
+            queueContents = new int[maxSize];
         }
 
         // method to add to queue
         public void Enqueue(int item)
         {
-
+            if (!IsFull())
+            {
+                rear = (rear+1)%maxSize;
+                queueContents[rear] = item;
+                numItems++;
+            }
+            else
+            {
+                Console.WriteLine("Queue is full!");
+            }
         }
 
         // method to remove from queue
         public int Dequeue()
         {
-
+            if (!IsEmpty())
+            {
+                int deQueuedValue = queueContents[front];
+                front = (front+1)%maxSize;
+                numItems--;
+                return deQueuedValue;
+            }
+            else
+            {
+                Console.WriteLine("Queue is empty!");
+                return 0;
+            }
         }
 
         // method to check if queue is full
         private bool IsFull()
         {
-
+            return numItems == maxSize;
         }
 
         // method to check if queue is empty
         private bool IsEmpty()
         {
-
+            return numItems == 0;
         }
 
         public void Display()
         {
-
+            for (int i = 0; i < maxSize; i++)
+            {
+                Console.Write($"{i}: {queueContents[i]}");
+                if (i == front)
+                {
+                    Console.Write("<-- front");
+                }
+                if (i == rear)
+                {
+                    Console.Write("<-- rear");
+                }
+                Console.WriteLine();
+            }
         }
     }
-}
-
